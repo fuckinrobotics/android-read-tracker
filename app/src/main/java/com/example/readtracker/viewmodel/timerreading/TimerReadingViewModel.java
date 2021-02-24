94c323ce1476c6b579eecca9e6b2f0225d5236b7
fuckinrobotics <60086412+fuckinrobotics@users.noreply.github.com>
2021-02-24 14:44:53 +0300

    Add timer and ui elements on TimerReadingFragment.

---
+++ app/src/main/java/com/example/readtracker/viewmodel/timerreading/TimerReadingViewModel.java
@@ -0,0 +1,18 @@
+package com.example.readtracker.viewmodel.timerreading;
+
+import androidx.lifecycle.ViewModel;
+
+public class TimerReadingViewModel extends ViewModel {
+    /*private MutableLiveData<List<User>> users;
+    public LiveData<List<User>> getUsers() {
+        if (users == null) {
+            users = new MutableLiveData<List<User>>();
+            loadUsers();
+        }
+        return users;
+    }*/
+
+    private void loadUsers() {
+        // Do an asynchronous operation to fetch users.
+    }
+}
